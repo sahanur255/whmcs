ec2ea64e7200b560ef8f32e2e37398a6cfc3e9fe
Sahanur Mondal <76404276+sahanur255@users.noreply.github.com>
2023-10-02 19:42:37 +0530

    Update AddMenu.php

--- AddMenu.php
+++ AddMenu.php
@@ -1,1 +1,10 @@
+<?php
 
+use WHMCS\View\Menu\Item as MenuItem;
+
+add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
+{
+    $primaryNavbar->addChild('Menu Name')
+        ->setUri('https://www.example.com/')
+        ->setOrder(70);
+});
