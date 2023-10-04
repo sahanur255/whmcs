1ecee936ba02c88e8fb365fdf0ff8489daa52d73
Sahanur Mondal <76404276+sahanur255@users.noreply.github.com>
2023-10-04 12:34:52 +0530

    Update RearrangeMenu.php

--- RearrangeMenu.php
+++ RearrangeMenu.php
@@ -1,1 +1,19 @@
+<?php
 
+use WHMCS\View\Menu\Item as MenuItem;
+
+add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
+{
+    $navItem = $primaryNavbar->getChild('Support');
+    if (is_null($navItem)) {
+        return;
+    }
+
+    $navItem = $navItem->getChild('Announcements');
+    if (is_null($navItem)) {
+        return;
+    }
+
+    $navItem->setOrder(1);
+
+});
