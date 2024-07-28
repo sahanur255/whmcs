3923439614d67fb79cc6e19d7bd9868a846196de
Sahanur Mondal <76404276+sahanur255@users.noreply.github.com>
2024-07-28 20:17:28 +0530

    Update cart_gateway_removal_for_free_products.php

--- cart_gateway_removal_for_free_products.php
+++ cart_gateway_removal_for_free_products.php
@@ -1,22 +1,38 @@
 <?php
 
 /**
-* Detect if cart total zero - if so, remove other gateways
-* @author Sahanur
-*/
- 
+ * Detect if cart total is zero - if so, show only allowed gateways
+ * If cart total is greater than zero, remove allowed gateways
+ * @autor Sahanur
+ */
+
 function cart_gateway_removal_for_free_products($vars)
 {
-	if ($vars['templatefile']=='viewcart'){
-		$gateways = $vars['gateways'];
-		$total = $vars['total']->toNumeric();
-		$allowed = ['mailin','paypal'];
-		foreach ($gateways as $k => $item) {
-			if (!in_array($item['sysname'],$allowed) and $total == '0') {
-				unset($gateways[$k]);
-			}
-		} 
-		return array("gateways" => $gateways);
-	}
+    if ($vars['templatefile'] == 'viewcart') {
+        $gateways = $vars['gateways'];
+        $total = $vars['total']->toNumeric();
+        $allowed = ['mailin'];
+
+        if ($total == 0) {
+            // Keep only the allowed gateways
+            foreach ($gateways as $k => $item) {
+                if (!in_array($item['sysname'], $allowed)) {
+                    unset($gateways[$k]);
+                }
+            }
+        } else {
+            // Remove the allowed gateways
+            foreach ($gateways as $k => $item) {
+                if (in_array($item['sysname'], $allowed)) {
+                    unset($gateways[$k]);
+                }
+            }
+        }
+
+        return array("gateways" => $gateways);
+    }
+    return $vars; // Ensure you return the original data if the condition is not met
 }
-add_hook("ClientAreaPageCart", 1, "cart_gateway_removal_for_free_products");
+
+// Register the function to the appropriate hook
+add_hook('ClientAreaPageCart', 1, 'cart_gateway_removal_for_free_products');
